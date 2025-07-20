fc8b1876961cf19fcaca6a93dd2f2fd10d3c0983
Joylinton04 <adujoy05@gmail.com>
2025-07-20 23:58:08 +0000

    initial assets

---
+++ src/assets/assets.ts
@@ -0,0 +1,14 @@
+import heroImg from './hero section.png'
+import logo from './logo.svg'
+import location from './location.svg'
+import guest from './guest.svg'
+
+
+
+
+export const assets = {
+    heroImg,
+    logo,
+    location,
+    guest,
+}
