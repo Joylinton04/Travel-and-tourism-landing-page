7a1d4c09b688bfc340a27879a4a585317c5da34d
Joylinton04 <adujoy05@gmail.com>
2025-07-24 09:36:17 +0000

    refactor

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import LandingPage from "./LandingPage";
 
 const App = () => {
   return (
-    <div className="h-[300vh] w-full">
+    <div className="h-[400vh] w-full">
       <LandingPage />
     </div>
   );
