ebc8e228af14353a312c22a01f410d3aa0895c62
Joylinton04 <adujoy05@gmail.com>
2025-07-27 22:55:07 +0000

    custom screens

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
@@ -13,8 +10,15 @@ export default {
       fontFamily: {
         body: ["Manrope", "sans-serif"],
         heading: ["Bricolage Grotesque", "sans-serif"],
-      }
+      },
+      screens: {
+        xxl: { max: "1280px" },
+        lgg: { max: "1024px" },
+        mdd: { max: "820px" },
+        ssm: { max: "700px" },
+        sssm: { max: "390px" },
+      },
     },
   },
   plugins: [],
-}
+};
